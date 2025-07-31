03632e7d2426ce8858048a979d1c89b7d290936b
calsmith99 <cs@reviews.io>
2025-07-31 02:15:30 +0100

    remove log

--- src/spotifyAuth.js
+++ src/spotifyAuth.js
@@ -6,9 +6,6 @@ const getSpotifyAccessToken = async () => {
 
   const tokenUrl = 'https://accounts.spotify.com/api/token';
   const credentials = btoa(`${clientId}:${clientSecret}`);
-    console.log('Client ID:', clientId);
-    console.log('Client Secret:', clientSecret);
-    console.log('Encoded credentials:', credentials);
 
   try {
     const response = await axios.post(
